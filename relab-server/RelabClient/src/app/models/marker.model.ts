b0759ae8cbcae4d4bc5a334b3d6b00d650177a0b
FioritoGiovanni <fiorito.giovanni@iisgalvanimi.edu.it>
2020-05-24 23:02:29 +0000

    finale

--- relab-server/RelabClient/src/app/models/marker.model.ts
+++ relab-server/RelabClient/src/app/models/marker.model.ts
@@ -7,48 +7,48 @@ export class Marker {
    //l’etichetta
     constructor(public lat: number, public lng: number, public label?: string)
     {
-        if (this.label.includes("Gas")) {
-            this.icon = new Icon ( './assets/img/gas.ico', 24 );
+        if (this.label.includes("Corona")) {
+            this.icon = new Icon ( './assets/img/corona.ico', 24 );
              this.label = "";
         }
-        if(this.label.includes("elettrica"))
+        if(this.label.includes("Elettricita"))
         {
-            this.icon = new Icon('./assets/img/electricity.ico',24 );
+            this.icon = new Icon('./assets/img/lampadina.ico',24 );
              this.label = "";
         }
-        if(this.label.includes("Gasolio"))
+        if(this.label.includes("OLIO"))
         {
-            this.icon = new Icon ('./assets/img/gasoline.ico',24 );
+            this.icon = new Icon ('./assets/img/oil.ico',24 );
              this.label = "";
         }
-        if(this.label.includes("Teleriscaldamento"))
+        if(this.label.includes("Riscaldamento"))
         {
-            this.icon = new Icon ( './assets/img/tele.ico',24 );
+            this.icon = new Icon ( './assets/img/caldo.ico',24 );
              this.label = "";
         }
-        if(this.label.includes("Olio"))
+        if(this.label.includes("GAS"))
         {
-            this.icon = new Icon('./assets/img/olio_com.ico',24);
+            this.icon = new Icon('./assets/img/gasOil.ico',24);
              this.label = "";
         }
         if(this.label.includes("GPL"))
         {
-            this.icon = new Icon ('./assets/img/gpl.ico',24 );
+            this.icon = new Icon ('./assets/img/GPL.ico',24 );
              this.label = "";
         }
         if(this.label.includes("Biomasse solide"))
         {
-            this.icon = new Icon ('./assets/img/bio.ico',24 );
+            this.icon = new Icon ('./assets/img/solide.ico',24 );
              this.label = "";
         }
         if(this.label.includes("Biomasse liquide"))
         {
-            this.icon = new Icon ('./assets/img/biomasseliquide.ico',24 );
+            this.icon = new Icon ('./assets/img/liquido.ico',24 );
              this.label = "";
         }
         if(this.label.includes("RSU"))
         {
-            this.icon = new Icon('./assets/img/rsu.ico',24 );
+            this.icon = new Icon('./assets/img/RSU.ico',24 );
              this.label = "";
         }
     }
